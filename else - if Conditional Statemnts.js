96b6d4af05280ad8205892010117ff4cb17d4836
Sushankhya-911 <loudersushankhya@gmail.com>
2025-01-24 17:19:15 +0545

    Add files via upload

---
+++ else - if Conditional Statemnts.js
@@ -0,0 +1,9 @@
+age=18
+if (age>=18){
+    console.log('You can vote')
+}else if (age<18){
+    console.log('You ar not eligible to vote')
+}else{
+    console.log('You are not resident of the country')
+}
+//else if can be use multiple times if there ae more than 2 conditions..
